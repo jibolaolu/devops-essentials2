99c58aafbf8d78ae1d953665be89e3152395b95c
Olusegun <olu@localhost.localdomain>
2019-10-26 21:40:46 +0100

    updated security policy

--- main.tf
+++ main.tf
@@ -39,8 +39,8 @@ resource "aws_security_group" "apps_allow" {
   }
 
   ingress {
-    from_port   = 8081
-    to_port     = 8081
+    from_port   = 8082
+    to_port     = 8082
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
