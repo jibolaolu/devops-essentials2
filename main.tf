31f1e37a48090a730e31ec29274e564db7dda2ca
EC2 Default User <ec2-user@ip-10-0-0-221.eu-west-2.compute.internal>
2019-10-30 19:31:23 +0000

    web-server

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
-data "template_file" "appserver-data" {
-  template = "${file("template/appserver-data.tpl")}"
+data "template_file" "webserver-data" {
+  template = "${file("template/webserver-data.tpl")}"
 }
 
-resource "aws_instance" "AppServerBox" {
+resource "aws_instance" "WebServerBox" {
   ami                         = "${var.ami_id}"
   instance_type               = "${var.instance_type}"
   associate_public_ip_address = true
   ebs_optimized               = false
   key_name                    = "${var.key_name}"
-  user_data                   = "${data.template_file.appserver-data.rendered}"
+  user_data                   = "${data.template_file.webserver-data.rendered}"
   #subnet_id                   = "${aws_subnet.public[0]}"
   subnet_id              = "${var.subnet_id}"
-  vpc_security_group_ids = ["${aws_security_group.apps_allow.id}"]
+  vpc_security_group_ids = ["${aws_security_group.web_allow.id}"]
 
   tags = {
-    Name = "appserverbox"
+    Name = "webserverbox"
   }
 }
 
-resource "aws_security_group" "apps_allow" {
-  name        = "app_allow_all"
+resource "aws_security_group" "web_allow" {
+  name        = "web_allow_all"
   description = "Allow all inbound traffic"
   vpc_id      = "${var.vpc_id}"
 
@@ -54,7 +54,7 @@ resource "aws_security_group" "apps_allow" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "AppServerBox SG"
+    Name = "WebServerBox SG"
   }
 }
 
